f284011e0eeddd8cd47cc1f5e1d3bc7101a316cf
ahmed abass <ahmedabasss1515@gmail.com>
2025-01-08 20:03:41 +0300

    add features page

--- src/app/(defaults)/(home)/page.tsx
+++ src/app/(defaults)/(home)/page.tsx
@@ -4,6 +4,7 @@ import { Home } from "./_components/_Home";
 import { Categories } from "./_components/_categories";
 import { DealSection } from "./_components/_DealSection";
 import { DealSection2 } from "./_components/_DealSection2";
+import { Features } from "./_components/Features";
 // import { About } from "./_components/_About";
 // import { OurService } from "./_components/OurService";
 // import GeneralTrade from "./_components/_GeneralTrade";
@@ -21,6 +22,7 @@ const Page = async () => {
       {/* <Categories /> */}
       {/* <DealSection /> */}
       <DealSection2 />
+      <Features />
       {/* <About />
       <OurService />
       <GeneralTrade />
