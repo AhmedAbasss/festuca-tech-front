f315a1ceb3a2cea7473d1a6bb665c9a44dbce9df
ahmed abass <ahmedabasss1515@gmail.com>
2025-02-17 13:42:26 +0300

    fix naming

--- src/app/(defaults)/(home)/page.tsx
+++ src/app/(defaults)/(home)/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import React, { useEffect } from "react";
-import { Features } from "./_components/Features";
+import { Features } from "./_components/_Features";
 import { useDispatch } from "react-redux";
 import { setPageTitle } from "@/store/themeConfigSlice";
 import { useTranslation } from "react-i18next";
-import { Section1Products } from "./_components/Section1Products";
-import { Section2Products } from "./_components/Section2Products";
-import { Section1 } from "./_components/section1";
-import { Categories } from "./_components/categories";
+import { Section1Products } from "./_components/_Section1Products";
+import { Section2Products } from "./_components/_Section2Products";
+import { Section1 } from "./_components/_Section1";
+import { Categories } from "./_components/_Categories";
 
 const Page = async () => {
   const { t } = useTranslation();
